36a1b19567b7ad602fe4f07eabe7e3a48a5b7f41
RayenTellissy <rayentellissy571@gmail.com>
2023-07-03 03:16:02 +0100

    last version for today

--- backend/routes/servers.js
+++ backend/routes/servers.js
@@ -1,5 +1,5 @@
 const router = require("express").Router()
-const { fetchByUser, fetch, createServer, leaveServer, joinServer, count } = require("../controllers/servers")
+const { fetchByUser, fetch, createServer, leaveServer, joinServer, count, deleteServer } = require("../controllers/servers")
 
 router.get("/fetchByUser/:id", fetchByUser)
 router.get("/fetch/:id", fetch)
@@ -9,5 +9,6 @@ router.post("/create/:id", createServer)
 router.post("/join", joinServer)
 
 router.delete("/leave", leaveServer)
+router.delete("/delete", deleteServer)
 
 module.exports = router
