c59c15c063b0a850452d919202cfcce80736dabf
RayenTellissy <rayentellissy571@gmail.com>
2023-09-03 20:56:03 +0100

    test

--- backend/middleware/sessionMiddleware.js
+++ backend/middleware/sessionMiddleware.js
@@ -5,12 +5,13 @@ const sessionMiddleware = session({
   secret: process.env.COOKIE_SEED,
   name: "sid",
   resave: false,
-  saveUninitialized: false,
+  saveUninitialized: true,
   store: MongoStore.create({ mongoUrl: process.env.MONGO_URL, dbName: "sessions" }), // creating a mongodb session store
   cookie: {
-    secure: false,
+    secure: process.env.ENVIRONMENT === "production" ? true : false,
     expires: 604800000, // expires in a week
     httpOnly: true,
+    sameSite: "none"
   },
 })
 
