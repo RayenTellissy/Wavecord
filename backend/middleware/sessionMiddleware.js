d3ab9dec1ed30a4ce4c2833e463109cd9d89e97e
RayenTellissy <rayentellissy571@gmail.com>
2023-09-03 20:58:13 +0100

    .

--- backend/middleware/sessionMiddleware.js
+++ backend/middleware/sessionMiddleware.js
@@ -11,7 +11,7 @@ const sessionMiddleware = session({
     secure: process.env.ENVIRONMENT === "production" ? true : false,
     expires: 604800000, // expires in a week
     httpOnly: true,
-    sameSite: "none"
+    sameSite: "lax"
   },
 })
 
