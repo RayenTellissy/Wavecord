8b4e386ca02eb85c95367939bf752944577746ea
RayenTellissy <rayentellissy571@gmail.com>
2023-09-27 02:49:17 +0100

    test deploy fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,4 @@
 import React from "react"
-import { useLocation } from "react-router-dom"
-import addNotification, { Notifications } from "react-push-notification"
 
 // common components
 import BugReport from "./utils/BugReport/BugReport"
@@ -8,29 +6,10 @@ import BugReport from "./utils/BugReport/BugReport"
 // Application Router
 import Routing from "./utils/Routing"
 
-// helper functions
-import returnLocation from "./utils/Helper/returnLocation"
-
 // default styling
 import "./App.css"
 
 const App = () => {
-  // const location = useLocation().pathname
-
-  // useEffect(() => {
-
-  // },[location])
-
-  const notify = () => {
-    addNotification({
-      title: "username",
-      message: "the message sent",
-      icon: "user image",
-      duration: 3000,
-      native: true,
-      onClick: () => console.log("navigate to conversation")
-    })
-  }
 
   return (
     <>
